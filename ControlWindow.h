6623fa5fbcbdafa930c5c748013b9a2904a64338
daigokk <88434718+daigokk@users.noreply.github.com>
2026-08-02 11:34:46 +0900

    Add files via upload

--- ControlWindow.h
+++ ControlWindow.h
@@ -8,19 +8,16 @@
 
 void ControlWindow(Cfg& cfg, Daq& daq) {
     ImGui::Begin("Control");
-    static float frequency = cfg.excitation.frequency;
-    static float amplitude = cfg.excitation.amplitude;
-    if(ImGui::SliderFloat("Frequency", &frequency, 1e3f, 100e3f)) {
-        cfg.excitation.frequency = frequency;
+    ImGui::Text("%s", cfg.status.serialNumber);
+    if(ImGui::SliderFloat("Frequency", &cfg.excitation.frequency, 1e3f, 100e3f)) {
         daq.wavegen(cfg.excitation.frequency, cfg.excitation.amplitude);
     }
-    if(ImGui::SliderFloat("Amplitude", &amplitude, 0.0f, 5.0f)) {
-        cfg.excitation.amplitude = amplitude;
+    if(ImGui::SliderFloat("Amplitude", &cfg.excitation.amplitude, 0.0f, 5.0f)) {    
         daq.wavegen(cfg.excitation.frequency, cfg.excitation.amplitude);
     }
     if(ImGui::Button("Defaults")) {
-        frequency = cfg.excitation.frequency = 100e3f;
-        amplitude = cfg.excitation.amplitude = 1.0f;
+        cfg.excitation.frequency = 100e3f;
+        cfg.excitation.amplitude = 1.0f;
         daq.wavegen(cfg.excitation.frequency, cfg.excitation.amplitude);
     }
     ImGui::End();
