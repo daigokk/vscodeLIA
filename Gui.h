0232f8879b906835d3f281ec28d613112a9c4dac
dk <daigokk@gmail.com>
2026-08-05 13:12:16 +0900

    GuiクラスのInitialize関数をGuiCfgクラスを使用するように変更し、ウィンドウの位置とサイズを設定できるように改善。ウィンドウの破棄処理を安全に行うよう修正。

--- Gui.h
+++ Gui.h
@@ -21,9 +21,25 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
         glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
 
+
+class GuiCfg {
+public:
+    class WindowPosition {
+    public:
+        int x = 100;
+        int y = 100;
+    } w_pos;
+    class WindowSize {
+    public:
+        int width = 1280;
+        int height = 720;
+    } w_size;
+};
+
+
 class Gui {
 public:
-    static GLFWwindow* Initialize(const char* title = "", const int width = 1280, const int height = 720) {
+    static GLFWwindow* Initialize(const char* title = "", const GuiCfg& cfg = GuiCfg()) {
         if (!glfwInit()) {
             std::cerr << "Failed to initialize GLFW" << std::endl;
             exit(EXIT_FAILURE);
@@ -33,12 +49,13 @@ public:
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
         const char* glsl_version = "#version 130";
-        GLFWwindow* window = glfwCreateWindow(width, height, "codeLIA - Dear ImGui", NULL, NULL);
+        GLFWwindow* window = glfwCreateWindow(cfg.w_size.width, cfg.w_size.height, "codeLIA - Dear ImGui", NULL, NULL);
         if (!window) {
             std::cerr << "Failed to create GLFW window" << std::endl;
             glfwTerminate();
             exit(EXIT_FAILURE);
         }
+        glfwSetWindowPos(window, cfg.w_pos.x, cfg.w_pos.y);
         glfwSetWindowTitle(window, title);
         glfwSetKeyCallback(window, key_callback);
         glfwMakeContextCurrent(window);
@@ -60,15 +77,17 @@ public:
         ImGui_ImplGlfw_Shutdown();
         ImPlot::DestroyContext();
         ImGui::DestroyContext();
-
-        glfwDestroyWindow(window);
+        if(window){
+            glfwDestroyWindow(window);
+            window = nullptr;
+        }
         glfwTerminate();
     }
 
     static void BeginFrame(GLFWwindow* window){
-        int display_w, display_h;
-        glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        glViewport(0, 0, width, height);
         glClearColor(0.10f, 0.12f, 0.15f, 1.00f);
         glClear(GL_COLOR_BUFFER_BIT);
 
