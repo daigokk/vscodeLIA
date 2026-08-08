ae9a689cab41892f963fda2e8fb65e8e0244036b
daigokk <88434718+daigokk@users.noreply.github.com>
2026-08-08 10:28:32 +0900

    Add files via upload

---
+++ Gui.cpp
@@ -0,0 +1,143 @@
+#include "Gui.h"
+#include <GLFW/glfw3.h>
+#include <GL/gl.h>
+#include <IMGUI/imgui.h>
+#include <IMGUI/imgui_internal.h>
+#include <IMGUI/imgui_impl_glfw.h>
+#include <IMGUI/imgui_impl_opengl3.h>
+#include <IMGUI/implot.h>
+
+#include <iostream>
+#include <fstream>
+
+static void error_callback(int error, const char* description)
+{
+    std::cerr << "Error: " << description << std::endl;
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+static void* WindowSettingsHandler_ReadOpen(ImGuiContext*, ImGuiSettingsHandler*, const char* name)
+{
+    // ini ファイル内の [GLFWWindow][Data] セクションを対象にする
+    if (strcmp(name, "Data") == 0)
+        return (void*)1;
+    return NULL;
+}
+
+static void WindowSettingsHandler_ReadLine(ImGuiContext*, ImGuiSettingsHandler*, void* entry, const char* line)
+{
+    GLFWwindow* window = (GLFWwindow*)ImGui::GetIO().UserData;
+    if (!window) return;
+
+    int x, y, w, h;
+    if (sscanf(line, "Pos=%d,%d", &x, &y) == 2)
+    {
+        glfwSetWindowPos(window, x, y);
+    }
+    else if (sscanf(line, "Size=%d,%d", &w, &h) == 2)
+    {
+        // 0以下の異常値でウィンドウが消えるのを防止
+        if (w > 100 && h > 100) 
+            glfwSetWindowSize(window, w, h);
+    }
+}
+
+static void WindowSettingsHandler_WriteAll(ImGuiContext*, ImGuiSettingsHandler* handler, ImGuiTextBuffer* buf)
+{
+    GLFWwindow* window = (GLFWwindow*)ImGui::GetIO().UserData;
+    if (!window) return;
+
+    int x, y, w, h;
+    glfwGetWindowPos(window, &x, &y);
+    glfwGetWindowSize(window, &w, &h);
+
+    // ini ファイルに書き出すフォーマットを出力
+    buf->appendf("[%s][%s]\n", handler->TypeName, "Data");
+    buf->appendf("Pos=%d,%d\n", x, y);
+    buf->appendf("Size=%d,%d\n", w, h);
+    buf->appendf("\n");
+}
+
+// ハンドラーの登録関数
+void RegisterGLFWWindowSettingsHandler(GLFWwindow* window)
+{
+    // 回避策として UserData に GLFWwindow のポインタを保持させる
+    ImGui::GetIO().UserData = window;
+
+    ImGuiSettingsHandler ini_handler;
+    ini_handler.TypeName = "GLFWWindow";
+    ini_handler.TypeHash = ImHashStr("GLFWWindow");
+    ini_handler.ReadOpenFn = WindowSettingsHandler_ReadOpen;
+    ini_handler.ReadLineFn = WindowSettingsHandler_ReadLine;
+    ini_handler.WriteAllFn = WindowSettingsHandler_WriteAll;
+    
+    ImGui::AddSettingsHandler(&ini_handler);
+}
+
+GLFWwindow* Gui::Initialize(const char* title) {
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    const char* glsl_version = "#version 130";
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "codeLIA - Dear ImGui", NULL, NULL);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    glfwSetWindowTitle(window, title);
+    glfwSetKeyCallback(window, key_callback);
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImPlot::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+    RegisterGLFWWindowSettingsHandler(window);
+    return window;
+}
+
+void Gui::Shutdown(GLFWwindow* window){ 
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImPlot::DestroyContext();
+    ImGui::DestroyContext();
+    if(window){
+        glfwDestroyWindow(window);
+        window = nullptr;
+    }
+    glfwTerminate();
+}
+
+void Gui::BeginFrame(GLFWwindow* window){
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+    glClearColor(0.10f, 0.12f, 0.15f, 1.00f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+}
+
+void Gui::EndFrame(GLFWwindow* window){
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+}
