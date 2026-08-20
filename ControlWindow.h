566d0fe26633a6b9d1225947cd3bcb308c50b3a9
dk <daigokk@gmail.com>
2026-08-20 15:34:08 +0900

    ControlWindow関数にDAQ接続切断時のエラーメッセージ表示を追加し、メインループからのエラーメッセージ表示コードを削除

--- ControlWindow.h
+++ ControlWindow.h
@@ -8,8 +8,21 @@
 
 // DAQの出力する波形(周波数、振幅)を制御する
 void ControlWindow(Config& cfg, Daq& daq) {
+    if(!cfg.status.isRun){
+        // DAQとの接続が切れたとき
+        ImGui::SetNextWindowFocus();
+        ImGui::PushStyleColor(ImGuiCol_::ImGuiCol_Border, ImVec4(1.0f, 0.0f, 0.0f, 1.0f)); // 赤
+    }
     if(ImGui::Begin("Control")){
-        ImGui::Text("%s", cfg.status.deviceSerial.c_str());
+        if(!cfg.status.isRun){
+            // DAQとの接続が切れたとき
+            ImGui::PushStyleColor(ImGuiCol_::ImGuiCol_Text, ImVec4(1.0f, 0.0f, 0.0f, 1.0f)); // 赤
+            ImGui::Text("Disconnected");
+            ImGui::PopStyleColor();
+        }
+        else{
+            ImGui::Text("%s", cfg.status.deviceSerial.c_str());
+        }
         if(ImGui::SliderFloat("Frequency", &cfg.ringBuffer.excitation.frequency, 1e3f, 100e3f)) {
             if(daq.device_data) daq.wavegen(cfg.ringBuffer.excitation.frequency, cfg.ringBuffer.excitation.amplitude);
         }
@@ -23,4 +36,8 @@ void ControlWindow(Config& cfg, Daq& daq) {
         }
     }
     ImGui::End();
+    if(!cfg.status.isRun){
+        // DAQとの接続が切れたとき
+        ImGui::PopStyleColor();
+    }
 }
